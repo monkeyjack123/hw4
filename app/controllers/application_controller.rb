da86312237d01f6c3cbaffa7d33c53eb414bbffd
monkeyjack123 <jack.pu@kellogg.northwestern.edu>
2026-03-15 21:14:30 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    if session["user_id"]
+      @current_user = User.find_by({ "id" => session["user_id"] })
+    else
+      @current_user = nil
+    end
   end
+
+  def require_login
+    if @current_user == nil
+      redirect_to "/login"
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "places", :action => "index" })
+
+  get("/login", { :controller => "sessions", :action => "new" })
+  get("/logout", { :controller => "sessions", :action => "destroy" })
+  get("/signup", { :controller => "users", :action => "new" })
+  
   resources "entries"
   resources "places"
   resources "sessions"
